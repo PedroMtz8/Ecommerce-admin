e9ccc6dc9092521de4298be58f96b4b6ed6632c7
Pedro Martinez <pedrocmartinez568@gmail.com>
2023-10-20 23:11:04 -0600

    billboard create form

--- src/app/(dashboard)/[storeId]/(routes)/billboards/client.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/client.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import { Plus } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { Plus } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 
 export default function BillboardClient() {
@@ -13,7 +13,7 @@ export default function BillboardClient() {
     <>
       <div className="flex items-center justify-between">
         <Heading title="Billboards (0) " description="Manage billboards for your store" />
-        <Button>
+        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="w-4 h-4 mr-2" />
           Add new
         </Button>
