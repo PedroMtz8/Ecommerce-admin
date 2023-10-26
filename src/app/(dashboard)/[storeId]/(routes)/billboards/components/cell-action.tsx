b0c97d6a084f9eb276d7b2165d4564107e10142f
PedroMtz8 <pedrocmartinez568@gmail.com>
2023-10-25 22:21:51 -0600

    drop down menu added

--- src/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,6 +1,15 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { BillboardColumn } from './columns';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
+import { MoreHorizontal } from 'lucide-react';
 
 interface CellActionProps {
   data: BillboardColumn;
@@ -8,5 +17,18 @@ interface CellActionProps {
 
 export function CellAction({ data }: CellActionProps) {
   console.log(data);
-  return <div>Action</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="w-8 h-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="w-4 h-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem>Hola</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
