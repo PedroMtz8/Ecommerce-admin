47ee28f40b3b78efc246d1c0b3c7ff036debb360
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-02-20 20:35:46 -0600

    fix eslint error

--- src/app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,19 +1,15 @@
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/lib/prismadb';
 
-import { ProductForm } from "./components/product-form";
+import { ProductForm } from './components/product-form';
 
-const ProductPage = async ({
-  params
-}: {
-  params: { productId: string, storeId: string }
-}) => {
+const ProductPage = async ({ params }: { params: { productId: string; storeId: string } }) => {
   const product = await prismadb.product.findUnique({
     where: {
       id: params.productId,
     },
     include: {
       images: true,
-    }
+    },
   });
 
   const categories = await prismadb.category.findMany({
@@ -34,18 +30,13 @@ const ProductPage = async ({
     },
   });
 
-  return ( 
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm 
-          categories={categories} 
-          colors={colors}
-          sizes={sizes}
-          initialData={product}
-        />
+        <ProductForm categories={categories} colors={colors} sizes={sizes} initialData={product} />
       </div>
     </div>
   );
-}
+};
 
 export default ProductPage;
